adfec6f5e963f30cfaa3b90b3c4626f4f34b749a
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-09-04 19:29:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
